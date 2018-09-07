4c25ce2baebaf0324ed7b4a1edf89b69a108258a
Natasha Cox <natashac1014@gmail.com>
2018-09-07 14:25:12 -0500

    first commit

---
+++ path---index-ce593aa3ad5549e62b01.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{418:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Gatsby Starter - Strata by HTML5 UP",description:"A Gatsby.js Starter based on Strata by HTML5 UP"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-ce593aa3ad5549e62b01.js.map
